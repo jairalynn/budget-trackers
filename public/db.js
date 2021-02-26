5f7dffaf5b92d414745e236dc37e59d7ef69e865
Jaira Arcilla <arcilla.jaira@gmail.com>
2021-02-26 15:10:49 -0800

    fixed db.js

--- public/db.js
+++ public/db.js
@@ -1,64 +1,57 @@
-// create a new db for "budget" db
 let db;
-
+// create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
-// create object store called "pending" and set autoIncrement to true
 request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
-
 request.onsuccess = function (event) {
-    db = event.target.results;
-    //checks if online before reading db
-    if (navigator.onLine) {
-        checkDatabase();
-    }
+  db = event.target.result;
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
 };
-
-//for request errors
 request.onerror = function (event) {
-    cconsole.log("err" + event.target.erroCode);
+  console.log("Woops! " + event.target.errorCode);
 };
-
 function saveRecord(record) {
-    //open a transaction on the pending object store in the budget db with readwrite access
-    const transaction = db.transaction(["pending"], "readwrite");
-    //access your pending object store
-    const store = transaction.objectStore("pending");
-    //add record to your store with add method
-    store.add(record);
+  // open a transaction on the pending object store in the budget db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // add record to your store with add method.
+  store.add(record);
 }
-
-//posts all pending transaction to mongodb when connection is back online
 function checkDatabase() {
-    //open a transaction on the pending object store in the budget db
-    const transaction = db.transaction(["pending", "readwrite"]);
-    //access pending object store
-    const store = transaction.objectStore("pending");
-    //get all records from the store and set to a variable
-    const getAll = store.getAll();
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json",
-            },
-        })
+  // open a transaction on the pending object store in the budget db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
         .then(response => response.json())
         .then(() => {
-            //if successful, open transaction on your pending db
-            const transaction = db.transaction(["pending"], "readwrite");
-            //access your pending object store
-            const store = transaction.objectStore("pending");
-            //clear all items in your store
-            store.clear();
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+          // access your pending object store
+          const store = transaction.objectStore("pending");
+          // clear all items in your store
+          store.clear();
         });
-    }};
-
+    }
+  };
 }
-//listen for app coming back online
+// listen for app coming back online
 window.addEventListener("online", checkDatabase);
