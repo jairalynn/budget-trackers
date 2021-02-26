4cac8a740569b236378147651321987fb0bcd206
Jaira Arcilla <arcilla.jaira@gmail.com>
2021-02-26 04:24:38 -0800

    service-worker.js finished

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,5 +20,49 @@ self.addEventListener("install", fucntion (evt) {
         );
 
          //tell browswer to activate this service worker immediately once it finished installing
-    )
-})
+         self.skipWaiting();
+        });
+
+        self.addEventListener("activate", function(evt) {
+            evt.waitUntil(
+                caches.keys().then(keyList => {
+                    return Promise.all(
+                        keyList.map(key => {
+                            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                                console.log("Removing old cache data", key);
+                                return caches.delete(key);
+                            }
+                        })
+                    );
+                })
+            );
+            self.clients.claim();
+});
+
+//fetch
+self.addEventListener("fetch", function (evt) {
+    //cache handle requests here
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
+});
